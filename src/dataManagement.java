af22fb5f61be655145c1e244fb310ff8b6ceaac3
nrtallon06 <nrtallon06@wacohi.net>
2022-02-05 10:02:48 -0600

    dataManagement updated

--- src/dataManagement.java
+++ src/dataManagement.java
@@ -3,19 +3,37 @@ import java.io.File;
 public class dataManagement {
     public static int createFolder(String folderLocation, String folderName) {
         //creates new file
-        File f1 = new File(folderLocation + "/" + folderName);
-        boolean testForFolder = f1.mkdirs();
-        //tests if folder has been created
-        if (testForFolder) {
-            return 1;
-            //tests if folder exists already
-        } else if (f1.exists()) {
-            return 2;
-        } else {
-            return -1;
+
+        if(folderLocation != null && folderName != null) {
+            String lastChar = folderName.substring(folderName.length() - 1);
+            if(lastChar == "/" || lastChar == "\\") {
+                File f1 = new File(folderLocation + folderName);
+                boolean testForFolder = f1.mkdirs();
+                if (testForFolder) {
+                    return 1;
+                    //tests if folder exists already
+                } else if (f1.exists()) {
+                    return 2;
+                } else {
+                    return -1;
+                }
+            } else {
+                File f1 = new File(folderLocation + "/" + folderName);
+                boolean testForFolder = f1.mkdirs();
+                if (testForFolder) {
+                    return 1;
+                    //tests if folder exists already
+                } else if (f1.exists()) {
+                    return 2;
+                } else {
+                    return -1;
+                }
+            }
         }
 
 
+
+        return -1;
     }
 }
 
