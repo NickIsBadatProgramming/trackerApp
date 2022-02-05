989dbad8d794c50944a8c988804b061700e6a69b
nrtallon06 <nrtallon06@wacohi.net>
2022-02-05 10:03:59 -0600

    dataManagement updated

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
     // uses dataManagement class with startup method to create new folder if one is necessary
     int startup = (dataManagement.createFolder("H:","robotTracker"));
     switch(startup) {
@@ -24,6 +21,5 @@ public class Main {
 
 
 
-    scanner.close();
     }
 }
